752c7e81197c8569e0603865e8d5fd1fa6ec5c0a
Gabriel Ferreira <43641763+GFerreira03@users.noreply.github.com>
2020-06-14 19:19:06 -0300

    Listagem dos itens do banco de dados

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+'use strict'
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 //Schemas
 const user = require('./user-model');
@@ -11,93 +14,110 @@ const categoryModel = require('./category-model');
 //Inicializando o express
 const app = express();
 app.use('/static', express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: true}))
-app.set('view-engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 //Estabelecendo parâmetros pra conectar
 var porta = 8000;
 var url = "mongodb+srv://genericUser:123456789abc@cluster0-eian9.gcp.mongodb.net/listadetarefas?retryWrites=true&w=majority"
 
 //Conexão
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}).then(client => {
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }).then(client => {
     console.log('Conectado ao banco de dados')
 }).catch((err) => {
     console.log('Erro ao conectar. Erro: ' + err)
 })
 
-app.listen(porta, () => 
-{
-    console.log('Em execução na porta: '+ porta)
+app.listen(porta, () => {
+    console.log('Em execução na porta: ' + porta)
 })
 
 //Login
 var usuarioLogado = {};
 var listaCategorias = {};
+var listadetarefas = [];
 app.post('/logar', (req, res) => {
-        user.findOne(
-        {$and:[
-            {nomeUsuario: req.body.usuario}, 
-            {senha: req.body.senha}]}, 
-            (err, usuario) => {
-                if (err) console.log(err);
-                if (usuario == null){
-                    res.render('index.ejs')
-                } else {  
-                    if(err) return console.log(err);
-                    usuarioLogado = usuario 
-                        //Usuário salvo
-                    categoryModel.find({nomeUsuario: usuarioLogado.nomeUsuario}, (err, categorias) => {
-                        if (err) console.log(err);
-                        if (categorias == null){
-                            listaCategorias = null;
-                            res.render('task.ejs', { usuarioTopo: usuarioLogado, listaCategorias: listaCategorias })
-                        } else {
-                            for (i=0; i<categorias.length; i++) {
-                                listaCategorias[i] = categorias[i]._categoria;
-                                if((i + 1) == categorias.length){
-                                    res.render('task.ejs', { usuarioTopo: usuarioLogado, listaCategorias: listaCategorias, categoriasQnt: i+1 })
-                                }
-                            }
-                            
-                        }
-                    })
-                    
-                
-        }
+    user.findOne(
+        {
+            $and: [
+                { nomeUsuario: req.body.usuario },
+                { senha: req.body.senha }]
+        },
+        (err, usuario) => {
+            if (err) console.log(err);
+            if (usuario == null) {
+                res.render('index.ejs')
+            }
+            if (err) return console.log(err);
+            usuarioLogado = usuario
+            //Usuário salvo
 
-    })
+            categoryModel.find({ nomeUsuario: usuarioLogado.nomeUsuario }, (err, categorias) => {
+                if (err) console.log(err);
+                listaCategorias = categorias;
+
+                task.find({ nomeUsuario: usuarioLogado.nomeUsuario }, (err, tarefas) => {
+                    if (err) console.log(err)
+                    listadetarefas = tarefas;
+
+                    res.render('task.ejs',
+                        {
+                            usuarioTopo: usuarioLogado,
+                            listaCategorias: listaCategorias,
+                            listadetarefas: listadetarefas
+                        })
+                })
+            })
+        })
 })
 
 //Logout
-app.post('/deslogar',(req,res)=>{
-    if (usuarioLogado = true){
+app.post('/deslogar', (req, res) => {
+    if (usuarioLogado = true) {
         usuario = null,
-        res.render('index.ejs')
+            res.render('index.ejs')
     }
 })
 
 //Nova tarefa
 app.post('/novaTarefa', (req, res) => {
+
     var tarefa = {
         nomeUsuario: usuarioLogado.nomeUsuario,
         titulo: req.body.titulo,
         descricao: req.body.descricao,
+        categoria: req.body.categoria,
         dataIni: new Date(),
-        dataFim: new Date(req.body.dataFim),
+        dataFim: new Date(req.body.dataConclusao),
         conclusao: 'Ativa'
     }
 
     task.collection.insertOne(tarefa, (err, result) => {
         if (err) console.log(err);
     })
+    
+    categoryModel.find({ nomeUsuario: usuarioLogado.nomeUsuario }, (err, categorias) => {
+        if (err) console.log(err);
+        listaCategorias = categorias;
 
-    res.render('task.ejs', {usuarioTopo: usuarioLogado.nome + " " + usuarioLogado.sobrenome + " - "+ usuarioLogado.nomeUsuario})
+        task.find({ nomeUsuario: usuarioLogado.nomeUsuario }, (err, tarefas) => {
+            if (err) console.log(err)
+            listadetarefas = tarefas;
+
+            res.render('task.ejs',
+                {
+                    usuarioTopo: usuarioLogado,
+                    listaCategorias: listaCategorias,
+                    listadetarefas: listadetarefas
+                })
+        })
+    })
 })
 
 //Nova categoria
-app.post('/novaCategoria', (req, res) =>{
-        var categoria = {
+app.post('/novaCategoria', (req, res) => {
+    var categoria = {
         nomeUsuario: usuarioLogado.nomeUsuario,
         _categoria: req.body.novaCategoria
     }
@@ -105,9 +125,29 @@ app.post('/novaCategoria', (req, res) =>{
     categoryModel.collection.insertOne(categoria, (err, result) => {
         if (err) console.log(err)
     })
+
+    categoryModel.find({ nomeUsuario: usuarioLogado.nomeUsuario }, (err, categorias) => {
+        if (err) console.log(err);
+        listaCategorias = categorias;
+
+        task.find({ nomeUsuario: usuarioLogado.nomeUsuario }, (err, tarefas) => {
+            if (err) console.log(err)
+            listadetarefas = tarefas;
+
+            res.render('task.ejs',
+                {
+                    usuarioTopo: usuarioLogado,
+                    listaCategorias: listaCategorias,
+                    listadetarefas: listadetarefas
+                })
+        })
+    })
 })
 
 app.get('/', (req, res) => {
-    
     res.render('index.ejs')
+})
+
+app.get('/carregarTarefas', (req, res) => {
+    res.redirect('back')
 })
